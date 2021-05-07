608e559cb03a433b94e5241604744f9a7133353b
akisblack <mavrommatisakis@gmail.com>
2021-05-07 08:59:31 +0300

    add script

---
+++ src/js/discord-tag.js
@@ -0,0 +1,9 @@
+function DiscordTag() {
+    alert("Copied to your clipboard\nIf it didn't work, my tag is akisblack#6969");
+    try {
+        navigator.clipboard.writeText("akisblack#6969");
+    }
+    catch (err) {
+        console.log("Clipboard copy failed, user agent:" + navigator.userAgent);
+    } 
+}
