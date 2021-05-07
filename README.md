ed16ab2175e4ab50704d23b8a0160345c723240e
akisblack <mavrommatisakis@gmail.com>
2021-05-07 14:39:37 +0300

    branching

---
+++ README.md
@@ -0,0 +1,11 @@
+## This is my old website, you can find the new one at github.com/akisblack/akisblack.github.io
+
+# akisblack.github.io
+
+### Just a website I use for testing and hosting stuff.
+
+## Domains
+
+#### https://akisblack.github.io
+#### https://www.akisblack.tk
+#### https://akisblack.tk
--- src/js/discord-tag.js
+++ src/js/discord-tag.js
@@ -1,5 +1,5 @@
-function DiscordTag() {
-    alert("Copied to your clipboard\nIf it didn't work, my tag is akisblack#6969");
+function copyDiscordTag() {
+    alert("Copied to your clipboard!\nIf it didn't work, my tag is akisblack#6969");
     try {
         navigator.clipboard.writeText("akisblack#6969");
     }
@@ -7,5 +7,3 @@ function DiscordTag() {
         console.log("Clipboard copy failed, user agent:" + navigator.userAgent);
     } 
 }
-
-//Thanks to Odyssey346 for this
