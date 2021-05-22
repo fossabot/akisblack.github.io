ed84c674b250178c76ef916a119bd9dd082c6869
akisblack <mavrommatisakis@gmail.com>
2021-05-22 20:01:49 +0300

    forgot to change the tag

--- src/js/discord-tag.js
+++ src/js/discord-tag.js
@@ -1,7 +1,7 @@
 function DiscordTag() {
-    alert("Copied to your clipboard\nIf it didn't work, my tag is akisblack#6969");
+    alert("Copied to your clipboard\nIf it didn't work, my tag is akisblack#5675");
     try {
-        navigator.clipboard.writeText("akisblack#6969");
+        navigator.clipboard.writeText("akisblack#5675");
     }
     catch (err) {
         console.log("Clipboard copy failed, user agent:" + navigator.userAgent);
